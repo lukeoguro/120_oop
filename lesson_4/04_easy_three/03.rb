7322af1d11fdf80813e836abfd5acbce64a52b8d
Luke Oguro <lukeoguro@gmail.com>
2021-07-07 21:17:44 +0900

    [+] Add exercises 1-7 in 04_easy_three

---
+++ lesson_4/04_easy_three/03.rb
@@ -0,0 +1,21 @@
+class AngryCat
+  def initialize(age, name)
+    @age  = age
+    @name = name
+  end
+
+  def age
+    puts @age
+  end
+
+  def name
+    puts @name
+  end
+
+  def hiss
+    puts "Hisssss!!!"
+  end
+end
+
+p AngryCat.new(1, "Martin")
+p AngryCat.new(3, "Daniel")
